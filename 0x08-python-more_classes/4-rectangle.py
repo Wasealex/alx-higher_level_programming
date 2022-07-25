847e8788ffbf8d23b3f0c26c47f09f55e30596c9
Wasealex <wasealex27@gmail.com>
2022-07-25 16:48:16 +0300

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -7,7 +7,6 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -56,7 +55,7 @@ class Rectangle:
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
-        return ("")
+            return ("")
 
         rect = []
         for i in range(self.__height):
@@ -69,4 +68,4 @@ class Rectangle:
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
-        return (rect)")"
+        return (rect)
