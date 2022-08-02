c6d964505b2b2ca3b2d9a61b4a0eaf9baf42f204
Wasealex <wasealex27@gmail.com>
2022-08-02 19:32:50 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""import sys module"""
+"""add all arguements to a Python list and save them to a file"""
 import sys
+
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
