6f30f2c0062f822db4ca66807eb40a280602bfd5
Wasealex <wasealex27@gmail.com>
2023-11-30 14:37:27 +0300

    fixed for 1 argument only

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,10 @@ if __name__ == "__main__":
     if no_of_arguments <= 1:
         print("0 arguments.")
     else:
-        print("{:d} arguments:".format(no_of_arguments - 1))
-        for i in range(1, no_of_arguments):
-            print("{:d}: {}".format(i, sys.argv[i]))
+        if no_of_arguments == 2:
+            print("{:d} argument:".format(no_of_arguments - 1))
+            print("1: {}".format(sys.argv[1]))
+        else:
+            print("{:d} arguments:".format(no_of_arguments - 1))
+            for i in range(1, no_of_arguments):
+                print("{:d}: {}".format(i, sys.argv[i]))
