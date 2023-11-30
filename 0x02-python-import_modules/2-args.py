18836fdfcfea9969ef06e814d6f4c2fae63c6d5f
Wasealex <wasealex27@gmail.com>
2023-11-30 14:30:41 +0300

    forgot the .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     no_of_arguments = len(sys.argv)
     if no_of_arguments <= 1:
-        print("0 arguments")
+        print("0 arguments.")
     else:
         print("{:d} arguments:".format(no_of_arguments - 1))
         for i in range(1, no_of_arguments):
