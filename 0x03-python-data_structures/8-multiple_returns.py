934abede2c6d4fb65822d7b5f0bc7d8417656747
Wasealex <wasealex27@gmail.com>
2023-12-03 12:26:52 +0300

    tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         return(0, None)
-
-    length = len(sentence)
-    first = sentence[0]
-    return (length, first)
+    else:
+        length = len(sentence)
+        first = sentence[0]
+        return (length, first)
