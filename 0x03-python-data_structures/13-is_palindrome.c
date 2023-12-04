9c2287ddfdef0e9c9cfe80f8e84d9467025e4310
Wasealex <wasealex27@gmail.com>
2023-12-04 23:36:12 +0300

    checking for palindrum

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ *is_palindrome - checks if a singly linked list is a palindrome
+ *@head: is a pointer pointing to the first node
+ *Return: 0 if it is not a palindrome
+ *1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = NULL, *temp = NULL;
+	int *array_data;
+	int count = 0;
+	int i;
+
+	current = temp = *head;
+	if (current == NULL)
+	{
+		return (1);
+	}
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	array_data = malloc(sizeof(int) * count);
+	if (array_data == NULL)
+		exit(EXIT_FAILURE);
+	for (i = 0; i < count; i++)
+	{
+		array_data[i] = current->n;
+		current = current->next;
+	}
+	for (i = count; i < count / 2; i++)
+	{
+		if (array_data[i] != array_data[count - i - 1])
+		{
+			free(array_data);
+			return (0);
+		}
+	}
+
+	free(array_data);
+	return (1);
+}
