7c6ff19b224aaaf53bd44106ab366786565e9b19
Wasealex <wasealex27@gmail.com>
2023-12-07 01:27:04 +0300

    decrease the char for pycodestyle

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda r: list(map(lambda element: element**2, r)), matrix))
+    return list(map(lambda r: list(map(lambda ele: ele**2, r)), matrix))
