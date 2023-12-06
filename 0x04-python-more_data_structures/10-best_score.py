92421c601c125dcadc5ddac16d01e4509ebfb213
Wasealex <wasealex27@gmail.com>
2023-12-06 22:06:22 +0300

    tryed sorted list

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,11 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    best_key = max(a_dictionary, key=lambda key: a_dictionary[key])
+    best_key = None
+    sorted_value = sorted(a_dictionary.values())
+    best_value = sorted_value[-1]
+    for key, value in a_dictionary.items():
+        if value == best_value:
+            best_key = key
+            break
     return best_key
