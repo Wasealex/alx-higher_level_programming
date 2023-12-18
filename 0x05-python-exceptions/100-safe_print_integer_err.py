63588a32d953307993c6c94d4652c52924fd01cb
Wasealex <wasealex27@gmail.com>
2023-12-18 17:28:26 +0300

    safer in stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError) as message:
+        print("Exception: {}".format(message), file=stderr)
+        return (False)
