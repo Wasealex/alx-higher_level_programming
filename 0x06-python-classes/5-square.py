70193b89a04d0d30a1c5003d8483eff8e7b8a952
Wasealex <wasealex27@gmail.com>
2023-12-19 15:10:49 +0300

    print #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""created a class named Square that adds a size as int"""
+
+
+class Square:
+    """initialize the class with a size attribute"""
+    def __init__(self, size=0):
+        """size must be an intiger and greater than zero"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """return size of square as property"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """validate value"""
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """area is square of one side"""
+        area = self.__size ** 2
+        return area
+
+    def my_print(self):
+        """prints # based on size given"""
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print('#', end='')
+                print()
