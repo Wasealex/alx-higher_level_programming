38d6c2f6b860e7f6dbb2a897626bc36b96e92a2b
Wasealex <wasealex27@gmail.com>
2023-12-19 15:38:48 +0300

    trial

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""created a class named Square that adds a size as int"""
+
+
+class Square:
+    """initialize the class with a size attribute"""
+    def __init__(self, size=0, position=(0, 0)):
+        """size must be an intiger and greater than zero"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """return size of square as property"""
+        return (self.__size)
+
+    @property
+    def position(self):
+        """return position as private property"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """validate poistion"""
+        self.__position = value
+        if (type(value) != tuple
+                or len(value) != 2 or value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    @size.setter
+    def size(self, value):
+        """validate value"""
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """area is square of one side"""
+        area = self.__size ** 2
+        return area
+
+    def my_print(self):
+        """prints # based on size given"""
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                for z in range(self.__position[0]):
+                    print(' ', end='')
+                for y in range(self.__size):
+                    print('#', end='')
+                print()
