699ee42da9d1cad3207097c8ff4feb041adad1bb
Wasealex <wasealex27@gmail.com>
2023-12-19 12:17:05 +0300

    created size attribute to class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is a size attribute"""
+
+
+class Square:
+    """creating a size method"""
+    def __init__(self, size):
+        """size variable assignment to class"""
+        self.size = size
