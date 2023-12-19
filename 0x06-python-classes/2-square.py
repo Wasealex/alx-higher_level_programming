9864fb22664c655f9548ec93997f0e7bab8549b2
Wasealex <wasealex27@gmail.com>
2023-12-19 12:34:10 +0300

    size sqaure with exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""created a class named Square that adds a size as int"""
+
+
+class Square:
+    """initialize the class with a size attribute"""
+    def __init__(self, size=0):
+        """size must be an intiger and greater than zero"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
