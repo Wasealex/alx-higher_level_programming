bdfc93ccefeb4fdfe4609233013dc67f446f46ae
Wasealex <wasealex27@gmail.com>
2024-01-08 17:26:21 +0300

    created MyList class and doctest for the class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""contains one class MyList()
+"""
+
+
+class MyList(list):
+    """a subclass of list that inherits
+    """
+
+
+    def print_sorted(self):
+        """prints sorted list
+        """
+        print(sorted(self))
