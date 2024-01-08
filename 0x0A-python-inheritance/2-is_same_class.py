13acdfb1c67a7db736136d833f0b3d393230bfc1
Wasealex <wasealex27@gmail.com>
2024-01-08 21:11:10 +0300

    created 2-is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a module contains one function, is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """function takes to arguments and compares them for having
+       exact instance and returns True or False
+    Args:
+         obj = the first parameter
+         a_class = the second parameter
+    Returns:
+           bool: True if obj and a_class have exact instance
+                 False if not
+    """
+    return (isinstance(obj, a_class))
