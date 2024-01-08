310703b34cd948b356b19b10ff702efc7b67d7bd
Wasealex <wasealex27@gmail.com>
2024-01-08 21:38:07 +0300

    created a method area()

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a module containing class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    A class with public attribute area
+    """
+    def area(self):
+        """
+        rase an exception when called
+        """
+        raise Exception("area() is not implemented")
