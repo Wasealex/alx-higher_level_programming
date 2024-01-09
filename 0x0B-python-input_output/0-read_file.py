03f7c75c6614a3d433f3597afc3c168ceefaad26
Wasealex <wasealex27@gmail.com>
2024-01-09 14:10:09 +0300

    modules doct

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""
+"""this is a module that contains one function, read_file
 """
 
 
 def read_file(filename=""):
-    """
+    """takes one arguments filename and opens it and reads line from
+       it
     """
     with open(filename) as f:
         for line in f:
