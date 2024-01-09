a1722fcca62c78e6b2b721e916568b3a5bdafbd1
Wasealex <wasealex27@gmail.com>
2024-01-09 16:59:40 +0300

    json file

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+"""a module who has function json
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """a function that representation of json
+    """
+    return json.dumps(my_obj)
