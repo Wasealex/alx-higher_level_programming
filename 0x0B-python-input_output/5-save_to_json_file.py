10515564309a100501aad7b973a806a217b23562
Wasealex <wasealex27@gmail.com>
2024-01-09 20:50:18 +0300

    created save_to_json_file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a module contains one function, save_to_json_file
+"""
+import json
+
+def save_to_json_file(my_obj, filename):
+    """function that writes objets to a file
+       json representation
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
