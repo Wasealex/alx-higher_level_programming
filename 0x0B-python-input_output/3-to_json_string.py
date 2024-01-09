ae19af63b05ce457526dc928cb991cb6d493d3c6
Wasealex <wasealex27@gmail.com>
2024-01-09 17:09:50 +0300

    trials

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,14 @@
 #!/usr/bin/pyhton3
 """
-a module who has function json
+a module who has function to_json_string
+that takes an object
 """
 import json
 
 
 def to_json_string(my_obj):
-    """a function that representation of json
+    """
+    a function that representation of json of an obj
+    returns json
     """
     return json.dumps(my_obj)
