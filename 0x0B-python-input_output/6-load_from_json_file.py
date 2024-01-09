f2a52dc9fc9d7d63788ea7e645cfa85ae3301849
Wasealex <wasealex27@gmail.com>
2024-01-09 21:44:59 +0300

    created load_from_json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a  module containg only one function load_from_json_file
+"""
+import json
+
+
+
+def load_from_json_file(filename):
+    """function that creats an obj from json file
+    """
+    with open(filename, encoding='utf-8') as f:
+        return json.load(f)
