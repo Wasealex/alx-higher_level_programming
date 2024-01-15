6006376d9790fc71ebc19b1877f5bc0a9925a957
Wasealex <wasealex27@gmail.com>
2024-01-15 15:04:23 +0300

    created method size() for class Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""
+"""a module that contains one class, Square()
 """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
+    """class Square that contains its attribute of square
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """
+        """inistantion of class Square
         """
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
+    @property
+    def size(self):
+        """get size from rectangle one side
         """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set size a value
+        """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """string representation of class Square
         """
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
                                                          self.x,
