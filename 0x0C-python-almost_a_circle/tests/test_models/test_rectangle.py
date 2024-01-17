62fd69ec6e600920504db05dab7756988cb3babb
Wasealex <wasealex27@gmail.com>
2024-01-17 10:42:47 +0300

    trial Rectangle(1, 2)

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,23 +6,24 @@ from models.base import Base
 import unittest
 
 
-class TestRectangle_instantiation(unittest.TestCase):
+class TestRectangleInstantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Rectangle class
     """
-    def test_rectangle_with_base(self):
-        self.assertIsInstance(Rectangle(10, 5), Base)
-
     def test_two_args(self):
         r1 = Rectangle(5, 10)
         r2 = Rectangle(10, 5)
         self.assertEqual(r1.id, r2.id - 1)
 
-    def test_four(self):
+    def test_four_args(self):
         r1 = Rectangle(2, 4, 6, 8)
         r2 = Rectangle(4, 3, 2, 1)
         self.assertEqual(r1.id, r2.id - 1)
 
-    def test_six(self):
+    def test_two_args_print(self):
+        r1 = Rectangle(1, 2)
+        self.assertEqual(str(r1), "[Rectangle] (12) 0/0 - 1/2")
+
+    def test_six_args(self):
         with self.assertRaises(TypeError):
             Rectangle(1, 2, 3, 1, 2, 3)
 
