e76137958137c5590b8bc891b9b142aa0653a84f
Wasealex <wasealex27@gmail.com>
2024-01-17 09:15:20 +0300

    trying unittest on Base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -14,8 +14,29 @@ class TestBase_instantiation(unittest.TestCase):
         b2 = Base()
         self.assertEqual(b1.id, b2.id - 1)
 
+    def test_three_base(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
+
     def test_unique_id(self):
         self.assertEqual(12, Base(12).id)
+
+    def test_id_after_unique_id(self):
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
+
+    def test_id_public(self):
+        b = Base(12)
+        b.id = 15
+        self.asserEqual(15, b.id)
+
+    def test_two_args(self):
+        with self.assertRaises(TypeError):
+            Base(3, 5)
 
 if __name__ == '__main__':
     unittest.main()
