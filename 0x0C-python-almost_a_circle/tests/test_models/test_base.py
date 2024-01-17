f6793302174efa70d33ba70a45dd6f0bd53bb5b0
Wasealex <wasealex27@gmail.com>
2024-01-17 09:59:29 +0300

    created unittest for Base instantation

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -38,5 +38,45 @@ class TestBase_instantiation(unittest.TestCase):
         with self.assertRaises(TypeError):
             Base(3, 5)
 
+    def test_unique_id(self):
+        self.assertEqual(12, Base(12).id)
+
+    def test_nb_instances_private(self):
+        with self.assertRaises(AttributeError):
+            print(Base(12).__nb_instances)
+
+    def test_id_str(self):
+        self.assertEqual("ALX", Base("ALX").id)
+
+    def test_id_float(self):
+        self.assertEqual(12.5, Base(12.5).id)
+
+    def test_id_complex(self):
+        self.assertEqual(complex(15), Base(complex(15)).id)
+
+    def test_id_dictionary(self):
+        self.assertEqual({"Alpha": 1, "B": 2}, Base({"Alpha": 1, "B": 2}).id)
+
+    def test_id_bool(self):
+        self.assertEqual(True, Base(True).id)
+
+    def test_id_list(self):
+        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
+
+    def test_bytes_id(self):
+        self.assertEqual(b'Python', Base(b'Python').id)
+
+    def test_bytearray_id(self):
+        self.assertEqual(bytearray(b'abcefg'), Base(bytearray(b'abcefg')).id)
+
+    def test_memoryview_id(self):
+        self.assertEqual(memoryview(b'abcefg'), Base(memoryview(b'abcefg')).id)
+
+    def test_inf_id(self):
+        self.assertEqual(float('inf'), Base(float('inf')).id)
+
+    def test_NaN_id(self):
+        self.assertNotEqual(float('nan'), Base(float('nan')).id)
+
 if __name__ == '__main__':
     unittest.main()
