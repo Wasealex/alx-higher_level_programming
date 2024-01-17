8a56aca6e66d78d05e54298b2876d8f383bb32c1
Wasealex <wasealex27@gmail.com>
2024-01-17 11:04:54 +0300

    created unittest for rectangle errors

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -23,9 +23,81 @@ class TestRectangleInstantiation(unittest.TestCase):
         r1 = Rectangle(1, 2)
         self.assertEqual(str(r1), "[Rectangle] (12) 0/0 - 1/2")
 
+    def test_two_args_print(self):
+        r1 = Rectangle(1, 2, 3)
+        self.assertEqual(str(r1), "[Rectangle] (13) 3/0 - 1/2")
+
+    def test_two_args_print(self):
+        r1 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(str(r1), "[Rectangle] (14) 3/4 - 1/2")
+
+    def test_two_args_print(self):
+        r1 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(str(r1), "[Rectangle] (5) 3/4 - 1/2")
+
     def test_six_args(self):
         with self.assertRaises(TypeError):
             Rectangle(1, 2, 3, 1, 2, 3)
+
+class TestRectangleErrors(unittest.TestCase):
+
+    def test_None_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(None, 2)
+
+    def test_str_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle("1", 2)
+
+    def test_zero_width(self):
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(0, 2)
+
+    def test_negative_width(self):
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(-1, 2)
+
+    def test_None_height(self):
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(2, None)
+
+    def test_str_height(self):
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(1, "2")
+
+    def test_zero_height(self):
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            Rectangle(2, 0)
+
+    def test_negative_height(self):
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            Rectangle(1, -2)
+
+    def test_None_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(1, 2, None)
+
+    def test_str_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(1, 2, "3", 4)
+
+    def test_negative_x(self):
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            Rectangle(5, 3, -1, 0)
+
+    def test_None_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(1, 2, 3, None)
+
+    def test_str_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(1, 2, 3, "4")
+
+    def test_negative_y(self):
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            Rectangle(5, 3, 1, -1)
+
+
 
 if __name__ == '__main__':
     unittest.main()
