d7a0e7b0d5fa4d3af88cbb8a0c78c60fa2023aaf
Wasealex <wasealex27@gmail.com>
2024-01-17 14:03:00 +0300

    display() method on unittest

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,6 +4,8 @@
 from models.rectangle import Rectangle
 from models.base import Base
 import unittest
+import sys
+from io import StringIO
 
 
 class TestRectangleInstantiation(unittest.TestCase):
@@ -40,7 +42,8 @@ class TestRectangleInstantiation(unittest.TestCase):
             Rectangle(1, 2, 3, 1, 2, 3)
 
 class TestRectangleErrors(unittest.TestCase):
-
+    """testing different values for Rectangle
+    """
     def test_None_width(self):
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
             Rectangle(None, 2)
@@ -97,6 +100,9 @@ class TestRectangleErrors(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "y must be >= 0"):
             Rectangle(5, 3, 1, -1)
 
+class TestRectangleArea(unittest.TestCase):
+    """testing area() method of class Rectangle
+    """
     def test_area_1(self):
         r1 = Rectangle(2, 4)
         self.assertEqual(8, r1.area())
@@ -104,6 +110,38 @@ class TestRectangleErrors(unittest.TestCase):
     def test_area_2(self):
         r1 = Rectangle(5, 10, 1, 5, 10)
         self.assertEqual(50, r1.area())
+class MyTest(unittest.TestCase):
+    """testing with helper method for printed outputs
+    """
+    def assertPrintedOutput(self, expected, test):
+        output = StringIO()
+        sys.stdout = output
+        test()
+        out = output.getvalue()
+        sys.stdout = sys.__stdout__
+
+class TestRectangleDisplay(MyTest):
+    """ unittest for testing the display() method of Rectangle
+    """
+    def test_display_width_height(self):
+        r1 = Rectangle(3, 3, 0, 0, 0)
+        self.assertPrintedOutput("###\n###\n###\n", r1.display)
+
+    def test_display_width_height_x(self):
+        r = Rectangle(3, 2, 1, 0, 1)
+        self.assertPrintedOutput(" ###\n ###\n", r.display)
+
+    def test_display_width_height_y(self):
+        r = Rectangle(4, 5, 0, 1, 0)
+        o = "\n####\n####\n####\n####\n####\n"
+        self.assertPrintedOutput(o, r.display)
+
+    def test_display_width_height_x_y(self):
+        r = Rectangle(2, 4, 3, 2, 0)
+        o = "\n\n   ##\n   ##\n   ##\n   ##\n"
+        self.assertPrintedOutput(o, r.display)
+
+
 
 if __name__ == '__main__':
     unittest.main()
