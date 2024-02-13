35572d37d08883e8faf4cedf9d47f6b180e73b84
Wasealex <wasealex27@gmail.com>
2024-02-13 13:12:01 -0800

    inserting a row

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- insert a new row in the table
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
+-- database name will passed as an argument of the mysql cmd
+SELECT COUNT(*) FROM first_table WHERE id=89;
