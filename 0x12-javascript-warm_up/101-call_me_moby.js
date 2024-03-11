6fe3306ed04a9a3c76ca27695be42f0dd0c183fb
Wasealex <wasealex27@gmail.com>
2024-03-11 23:59:13 +0300

    semistandard fix

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction){
-    for (let idx = 0; idx < x; idx++){
-	theFunction()
-    }
-}
+exports.callMeMoby = function (x, theFunction) {
+  for (let idx = 0; idx < x; idx++) {
+    theFunction();
+  }
+};
