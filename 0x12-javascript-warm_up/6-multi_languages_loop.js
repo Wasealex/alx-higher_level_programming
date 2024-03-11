68dd2f199cd3de52fc9bf8dcdf15872a4c3a3a72
Wasealex <wasealex27@gmail.com>
2024-03-11 17:04:05 +0300

    loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let index = 0; index < strings.length; index++) {
+  console.log(strings[index]);
+}
