a1347185a7aeda9bbb2f6c20203326b5f4c21d6e
Wasealex <wasealex27@gmail.com>
2024-03-17 13:49:29 +0300

    ordering

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists all State objects from the database `hbtn_0e_6_usa`.
+"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    instance = session.query(State).order_by(State.id).first()
+    if instance is None:
+        print("Nothing")
+    else:
+        print("{:d}: {}".format(instance.id, instance.name))
